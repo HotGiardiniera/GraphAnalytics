7a2170857147ebd954ed27bdfce4da7aed911a09
chris <chris.moirano@gmail.com>
2018-04-12 23:04:00 -0400

    Crude parallel page rank speedup

--- pagerank/ppagerank.cpp
+++ pagerank/ppagerank.cpp
@@ -4,20 +4,21 @@
 #include <vector>
 #include <queue>
 #include "PageNode.h"
+#include <omp.h>
 
 
 int main(int argc, char *argv[]){
 
-    if(argc < 2){
-        printf("Invalid parameters. Usage: ./pagerank inputfile\n");
+    if(argc < 3){
+        printf("Invalid parameters. Usage: ./pagerank inputfile thread_count\n");
         return EXIT_FAILURE;
     }
 
-    float dapening_f = 0.85;  // TODO take as an input 
+    float dapening_f = 0.85; 
 
     // -----  Program args  -----------------
-    int file_val, m_size;
-    char *file_name = argv[argc-1];
+    int file_val, m_size, thread_count = atoi(argv[argc-1]);
+    char *file_name = argv[argc-2];
 
     FILE * file_pointer = NULL;
     file_pointer = fopen(file_name, "r");
@@ -63,19 +64,33 @@ int main(int argc, char *argv[]){
         }
     }
 
-    // iterate calculate our ranks
-    int iteration_count = 45;
-    float running_rank_sum;
+   // iterate calculate our ranks
+    int iteration_count = 45; 
+    float running_rank_sum = 0.0;
+    float *interation_vector = (float*)calloc(m_size, sizeof(float));
+
+    // BELOW CODE IS WORKING AND MAY WORK FASTER; WORSE COME TO WORSE WE HAVE A MULTITHREADED VERSION
 
     while(iteration_count > 0){
+
         for(int i=0; i<m_size; i++){
-            temp = &G[i];
             running_rank_sum = 0.0;
-            for(size_t j=0; j<page_ref_hash[i].size(); j++){  // find things that reference me
-                temp_ref = page_ref_hash[i][j];
-                running_rank_sum += (temp_ref->rank)/temp_ref->outdegree;
-            }
-            temp->rank = (1 - dapening_f) + dapening_f * running_rank_sum;
+            #pragma omp parallel num_threads(thread_count) reduction(+: running_rank_sum)
+            {   
+                PageNode *local_ref;
+                int num_threads = omp_get_num_threads();
+                int thread_id = omp_get_thread_num();
+                for(size_t j=thread_id; j<page_ref_hash[i].size(); j+=num_threads){  // find things that reference me
+                    local_ref = page_ref_hash[i][j];
+                    running_rank_sum += (local_ref->rank)/local_ref->outdegree;
+                }
+            } 
+            interation_vector[i] = (1 - dapening_f) + dapening_f * running_rank_sum;
+        }
+
+        #pragma omp parallel for num_threads(thread_count)
+        for(int i=0; i<m_size; i++){
+            G[i].rank = interation_vector[i];
         }
 
         iteration_count--;
@@ -85,7 +100,6 @@ int main(int argc, char *argv[]){
     for(int i=0; i<m_size; i++){
         printf("Page: %d rank: %f\n", G[i].name, G[i].rank);
     }
-
 
     return EXIT_SUCCESS;
 }
